2ba04d59bda3005bd19dd631b1f47f411b08467e
VitiukP <pasha2002vityuk@gmail.com>
2023-10-04 22:37:28 +0300

    update

---
+++ src/js/swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  // Navigation arrows
+  navigation: {
+    // nextEl: '.swiper-button-next',
+    // prevEl: '.swiper-button-prev',
+
+    nextEl: '.section-hero__button-right',
+    prevEl: '.section-hero__button-left',
+  },
+});
